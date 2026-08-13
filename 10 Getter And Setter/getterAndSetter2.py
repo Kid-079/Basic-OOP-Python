8aacd2b7d05fdc2b932ccf9a18670897dab645cb
Kid-079 <92396121+Kid-079@users.noreply.github.com>
2026-08-13 10:47:35 +0700

    Update getterAndSetter2.py

--- 10 Getter And Setter/getterAndSetter2.py
+++ 10 Getter And Setter/getterAndSetter2.py
@@ -4,7 +4,7 @@ class Food:
 		self.__foodName = inputFoodName
 		self.__health = inputHealth
 		self.__flavor = inputFlavor
-		self.__foodName = inputSpicyLevel
+		self.__spicy = inputSpicyLevel
 		self.info = "Food Name {} : \n\tSpicy Level {}".format(self.__foodName, self.__spicy)
 
 	def spicyLevel(self):
